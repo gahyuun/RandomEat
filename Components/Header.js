6f841c0e3454ed55df28832bb5d4c981f03c561d
89882 <pgh123166@naver.com>
2023-06-02 12:31:58 +0900

    chore: 화면 이동 버튼 추가

--- Components/Header.js
+++ Components/Header.js
@@ -1,10 +1,17 @@
-import backIcon from "../assets/backIcon.svg";
-import { TouchableOpacity } from "react-native";
-import { WithLocalSvg } from "react-native-svg";
+import backIcon from '../assets/backIcon.svg';
+import { Button, TouchableOpacity } from 'react-native';
+import { WithLocalSvg } from 'react-native-svg';
+import { Routes } from '../navigator/Routes';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Header() {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate(Routes.MAIN);
+      }}
+    >
       <WithLocalSvg asset={backIcon} />
     </TouchableOpacity>
   );
