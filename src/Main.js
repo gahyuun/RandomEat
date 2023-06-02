80291204679b964cd1f6611ab36eab4ffe421ef3
89882 <pgh123166@naver.com>
2023-06-02 15:52:05 +0900

    feat: 메인페이지 구현

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,56 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button, View } from 'react-native';
+import CategoryMenu from '../components/main/CategoryMenu';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import { Routes } from '../navigator/Routes';
+import RecommendMenu from '../assets/RecommendMenu.png';
 
 export default function Main() {
   const navigation = useNavigation();
   return (
     <View
       style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flex: 0.95,
       }}
     >
+      <View>
+        <Text style={[styles.headerText, styles.color]}>RANDOM EAT</Text>
+        <View style={styles.recommendContainer}>
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+            }}
+          >
+            <View
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={[styles.weatherText, styles.color]}>26℃</Text>
+              <Text
+                style={{
+                  fontWeight: 400,
+                  fontSize: 20,
+                  marginLeft: 30,
+                  marginTop: 20,
+                }}
+              >
+                오늘 같은 더운 날씨 {'\n'}빙수 어떤가요?
+              </Text>
+            </View>
+            <Image source={RecommendMenu}></Image>
+          </View>
+        </View>
+      </View>
+      {/* 프로젝트명 및 추천 메뉴 */}
+      <CategoryMenu></CategoryMenu>
+      {/* 카테고리별 메뉴 추천 */}
       <Button
-        title='Select'
+        title='룰렛 돌리러 가기'
         onPress={() => {
           navigation.navigate(Routes.SELECT);
         }}
@@ -21,3 +58,27 @@ export default function Main() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  color: {
+    color: '#693894',
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 60,
+    marginLeft: 40,
+  },
+  recommendContainer: {
+    backgroundColor: '#F6F3FF',
+    borderRadius: 40,
+    width: 341,
+    height: 187,
+    marginLeft: 25,
+    marginTop: 20,
+  },
+  weatherText: {
+    fontWeight: 700,
+    fontSize: 48,
+    marginTop: 20,
+  },
+});
