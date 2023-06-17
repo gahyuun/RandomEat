f3d0c45fbc7e6605398fb488550f3f11167b3204
hyunjin <tlsguswls319@naver.com>
2023-06-17 19:27:00 +0900

    feat:로딩화면 구현

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'react-native';
-import Navigation from './navigator/Navigation';
-import { RecoilRoot } from 'recoil';
+import { StatusBar } from "react-native";
+import Navigation from "./navigator/Navigation";
+import { RecoilRoot } from "recoil";
+import React, { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      await SplashScreen.preventAutoHideAsync();
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await SplashScreen.hideAsync();
+    };
+
+    hideSplashScreen();
+  }, []);
+
   return (
     <>
       <RecoilRoot>
-        <StatusBar style='dark' />
+        <StatusBar style="dark" />
         <Navigation />
       </RecoilRoot>
     </>
